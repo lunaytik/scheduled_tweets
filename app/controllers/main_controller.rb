20fb2fb9077baf59cc724e33c12159a2b2744783
lunaytik <nathandeuil13@gmail.com>
2022-12-19 09:21:48 +0100

    Add flash msgs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,7 @@
 class MainController < ApplicationController
-  
+  def index
+    #flash.now only render on current page
+    flash[:notice] = "Logged in successfully"
+    flash[:alert] = "Fail"
+  end
 end
