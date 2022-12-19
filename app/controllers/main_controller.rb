44039ebed7581c8a9d0aca12dda7767facf2891d
lunaytik <nathandeuil13@gmail.com>
2022-12-19 19:52:10 +0100

    Project

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     #flash.now only render on current page
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Fail"
+    #flash[:notice] = "Logged in successfully"
+    #flash[:alert] = "Fail"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@
 # password_confirmation virtual attr
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
+  has_many :tweets
+
   has_secure_password
 
   validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address"}
